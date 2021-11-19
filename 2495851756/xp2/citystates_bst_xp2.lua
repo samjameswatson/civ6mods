e299439ba304cc6214162c65740104478d8185ae
Sam Watson <swatson@lyles-sutherland.co.uk>
2021-11-19 17:54:55 +0000

    remove some unused mods

--- 2495851756/xp2/citystates_bst_xp2.lua
+++
@@ -1,6 +0,0 @@
--- ===========================================================================
--- Better City States
--- Author: Infixo
--- This file is needed for compatibility with CQUI
--- ===========================================================================
-include("CityStates_Expansion2");
